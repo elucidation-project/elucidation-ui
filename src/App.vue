fe69965cd5d74bd93f3a2184fd8ec6094cefc7aa
Dan Smith <dsmith@Ferb.local>
2020-07-22 13:46:55 -0400

    - added refresh button & loading mask - added global CSS file

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,7 @@
   <div id="app">
     <el-container class="elucidation-app">
       <el-aside class="elucidation-app-left">
-        <el-container>
-          <el-header class="elucidation-app-header">
-            <span>Services</span>
-          </el-header>
-          <el-main>
-            <service-list v-on:service-selected="onServiceSelected">
-            </service-list>
-          </el-main>
-        </el-container>
+        <service-list v-on:service-selected="onServiceSelected"></service-list>
       </el-aside>
       <el-container class="elucidation-app-center">
         <el-main>
@@ -52,61 +44,3 @@ export default {
   }
 };
 </script>
-
-<style>
-body, html {
-  height: 100%;
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 100%;
-  background-color: #777;
-
-}
-
-.elucidation-app .el-main,
-.elucidation-app .el-footer,
-.elucidation-app .el-header {
-    padding: 0;
-}
-
-.el-container {
-  padding: 5px;
-}
-
-.el-container.elucidation-app-center {
-  padding: 0 0 2px 5px;
-}
-
-.el-container:not(.custom-height) {
-  height: 100%;
-}
-
-.elucidation-app .identifier-details {
-  border: 0;
-}
-
-.elucidation-app .elucidation-app-header,
-.elucidation-app .identifier-details .el-collapse-item__header {
-  background-color: #4682B4; /* steel blue */
-  color: #fff;
-  font-weight: bold;
-  font-size: 1em;
-  height: 30px !important;
-  padding: 2px 10px;
-  text-align: left;
-  vertical-align: middle;
-}
-
-.elucidation-app-center .elucidation-app-relationships,
-.elucidation-app-left .el-container {
-  padding: 0;
-}
-
-</style>
